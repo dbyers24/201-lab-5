3b8a8307d88290741402044b43e88a9d2f1bf162
Nicholas Carignan <nick.carignan@sbcglobal.net>
2017-09-22 14:06:47 -0700

    question 3

--- app.js
+++ app.js
@@ -54,12 +54,16 @@ IMPORTANT DETAIL: You may not use the arithmetic operators + and * in this funct
 Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testSumAndMultiply() function and see if the test passes.*/
 
 // TODO: Write your code here
-function sumAndMultiply(a,b,c){ //eslint-disable-line
-
+function sumAndMultiply(a, b, c){ //eslint-disable-line
+  var theSum = sum(sum(a, b)[0], c)[0];
+  var product = multiply( multiply(a, b)[0], c)[0];
+  var message = a + ' and ' + b + ' and ' + c + ' sum to ' + theSum + '.';
+  var message2 = 'The product of ' + a + ' and ' + b + ' and ' + c + ' is ' + product + '.';
+  return [theSum, product, message, message2];
 }
 
 // TODO: Here is the test for sumAndMultiply(); uncomment it to run it
-// testSumAndMultiply(4,7,5);
+testSumAndMultiply(4,7,5);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
@@ -81,7 +85,7 @@ function sumArray(testArray){ //eslint-disable-line
 }
 
 // TODO: Here is the test for sumArray(); uncomment it to run it
-// testSumArray(testArray);
+testSumArray(testArray);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
